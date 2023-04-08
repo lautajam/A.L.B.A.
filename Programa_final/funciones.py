1035ec654e77876098f0cf3ea68086e79daaeed7
Lautaro <lautajamdev@gmail.com>
2023-04-08 00:42:26 -0300

    busca lo pedido por voz y lo lee

---
+++ Programa_final/funciones.py
@@ -0,0 +1,93 @@
+import threading
+import webbrowser
+from googlesearch import search
+import requests
+from bs4 import BeautifulSoup
+import pyttsx3
+import speech_recognition as sr
+
+def onClick_detener():
+    print("Lectura detenida")
+
+def busquedaLectura(text):
+    # Crear un objeto Recognizer
+    r = sr.Recognizer()
+
+    # Inicializar el motor de texto a voz
+    engine = pyttsx3.init()
+
+    # Configurar la voz
+    voices = engine.getProperty('voices')
+    engine.setProperty('rate', 150)
+    engine.setProperty('volume', 1)  # Volumen de la voz (valor entre 0 y 1)
+    
+    # Obtén el primer resultado de la búsqueda
+    result = next(search(text, num_results=1))
+
+    print(result)
+
+    # Hacer la petición HTTP y obtener el contenido de la página
+    response = requests.get(result)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Obtener los párrafos de la página
+    paragraphs = soup.find_all('p')
+
+    # Abre la página en Brave
+    webbrowser.register('brave', None, webbrowser.BackgroundBrowser("C://Program Files//BraveSoftware//Brave-Browser//Application//brave.exe"))
+    webbrowser.get('brave').open(result)
+
+    # Leer los párrafos en voz alta hasta que aparezca otra etiqueta
+    for paragraph in paragraphs:
+        text = paragraph.get_text()
+        engine.say(text)
+        
+        # Verificar si hay otra etiqueta después del párrafo actual
+        next_sibling = paragraph.find_next_sibling()
+        if next_sibling is not None and next_sibling.name != 'p':
+            break
+
+    # Ejecutar la lectura
+    engine.runAndWait()
+    pass
+
+def onClick_escuchar(resultado, escuchando):
+    print("Escuchando")
+    escuchando.config(text="Escuchando")  # muestra el texto escuchado en el label "resultado"
+    escuchando.update()
+
+    # Crear un objeto Recognizer
+    r = sr.Recognizer()
+    audio = ""
+
+    while 1:
+        # Utilizar el micrófono como fuente de audio
+        with sr.Microphone() as source:
+            print("Di algo:")
+            audio = r.listen(source, timeout=500)
+
+        # Intentar transcribir el audio
+        try:
+            text = r.recognize_google(audio, language='es-ES')
+            print("Transcripción: ", text)
+            resultado.config(text="Transcripción: " + text)  # muestra el texto escuchado en el label "resultado"
+            resultado.update()  # actualiza el label para que muestre el texto inmediatamente
+            busquedaLectura(text)
+            if "stop" == text.lower() or "estop" == text.lower() or "es top" == text.lower():
+                print("Escucha detenida")
+                resultado.config(text="Transcripción: " + text)  # muestra el texto escuchado en el label "resultado"
+                resultado.update()  # actualiza el label para que muestre el texto inmediatamente
+                escuchando.config(text="Escucha detenida")
+                escuchando.update()
+                break
+        except sr.UnknownValueError as e:
+            print("No te he entendido bien!")
+            resultado.config(text="No te he entendido bien!")
+            resultado.update()
+        except sr.RequestError as e:
+            print("Error al conectarse con el servicio de reconocimiento de voz; {0}".format(e))
+
+def start_listening(resultado, escuchando):
+    # Crear un hilo para ejecutar la función onClick_escuchar
+    t = threading.Thread(target=onClick_escuchar, args=(resultado,escuchando,))
+    t.start()
